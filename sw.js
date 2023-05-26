4cb81ab4ad739e70f7710805f0a52c0c6916bf3d
Bstsai <cookiestsai@gmail.com>
2023-05-26 16:35:55 +0200

    app

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = `geocoder-v1`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/lib/proj4leaflet.js',
+      '/lib/leaflet.wms.js',
+      '/lib/geojson.wfs.js',
+      '/lib/events.wfs.js',
+      '/lib/insert.wfs.js',
+      '/main.js',
+      '/main.css'
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
